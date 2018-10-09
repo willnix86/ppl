0fef83a4dc6398d7baa89e953026f0a67154c28c
Will Nixon <will_nixon@me.com>
2018-10-09 12:27:36 -0700

    added additonal static pages

--- test/test-api-endpoints.js
+++ test/test-api-endpoints.js
@@ -7,11 +7,39 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Load HTML', function() {
-    it('should load HTML successfully', function() {
+describe('Load static pages', function() {
+    it('should load the landing page HTML successfully', function() {
         let res;
         chai.request('http://localhost:8080')
         .get('/')
+        .then((_res) => {
+            res = _res;
+            res.should.have.status(200);
+            res.should.be.html;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    })
+
+    it('should load the user admin page HTML successfully', function() {
+        let res;
+        chai.request('http://localhost:8080')
+        .get('/user')
+        .then((_res) => {
+            res = _res;
+            res.should.have.status(200);
+            res.should.be.html;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    })
+
+    it('should load the person profile page HTML successfully', function() {
+        let res;
+        chai.request('http://localhost:8080')
+        .get('/person')
         .then((_res) => {
             res = _res;
             res.should.have.status(200);
